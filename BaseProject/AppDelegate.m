58e480187b4ae32d46e21612d1916ae66d7dee1c
bigcd <625554343@qq.com>
2015-11-07 09:24:19 +0800

    新闻页，科技、财经、体育、娱乐、头条Model解析完毕并调试成功

--- BaseProject/AppDelegate.m
+++ BaseProject/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "AppDelegate+Category.h"
 #import "VideoNetManager.h"
+#import "NewsNetManager.h"
 @interface AppDelegate ()
 
 @end
@@ -18,14 +19,26 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [self initializeWithApplication:application];
-//    [VideoNetManager getVideoWithIndex:0 completionHandle:^(id model, NSError *error) {
-//        DDLogVerbose(@"...");
-//    }];
-    
-    
-    
-    
-    
+    /*
+    [VideoNetManager getVideoWithIndex:0 completionHandle:^(id model, NSError *error) {
+        DDLogVerbose(@"...");
+    }];
+    [NewsNetManager getSEWithType:FirstTypeScience index:20 completionHandle:^(id model, NSError *error) {
+        DDLogVerbose(@"...");
+    }];
+    [NewsNetManager getSEWithType:FirstTypeEconomics index:20 completionHandle:^(id model, NSError *error) {
+        DDLogVerbose(@"...");
+    }];
+    [NewsNetManager getHSYWithType:SecondTypeYuLe index:20 completionHandle:^(id model, NSError *error) {
+        DDLogVerbose(@"...");
+    }];
+    [NewsNetManager getHSYWithType:SecondTypeSports index:20 completionHandle:^(id model, NSError *error) {
+        DDLogVerbose(@"...");
+    }];
+    [NewsNetManager getHSYWithType:SecondTypeHeadLine index:0 completionHandle:^(id model, NSError *error) {
+        DDLogVerbose(@"...");
+    }];
+     */
     
     
     
